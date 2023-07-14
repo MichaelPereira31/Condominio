1725f79da0f75f23ce335f978d84ed0d07c4e005
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-07-13 21:11:18 -0300

    feat: add app errors

--- src/shared/errors/getErrors.ts
+++ src/shared/errors/getErrors.ts
@@ -15,12 +15,14 @@ export function getErrors(
   });
 
   if (error instanceof AppError) {
-    return response
-      .status(error.statusCode)
-      .json({ status: 'error', message: error.message });
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
   }
 
-  return response
-    .status(500)
-    .json({ status: 'error', message: 'Internal Server Error' });
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
 }
