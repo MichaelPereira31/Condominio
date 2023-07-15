0c400152ec3572d979a8a71ded4a87e9958f54fa
Michael Pereira <michael.pereira@chat2desk.com.br>
2023-07-15 20:11:10 -0300

    refactor: refactoring create batch

--- src/modules/batch/useCases/create/CreateBatchUseCase.ts
+++ src/modules/batch/useCases/create/CreateBatchUseCase.ts
@@ -2,14 +2,17 @@ import { inject, injectable } from 'tsyringe';
 
 import { ICreateBatchDTO } from '@modules/batch/dtos/ICreateBatchDTO';
 import { IBatchRepository } from '@modules/batch/implementations/IBatchRepository';
+import { AppError } from '@shared/errors/AppError';
 
 @injectable()
 export class CreateBatchUseCase {
   constructor(
     @inject('BatchRepository')
     private readonly batchRepository: IBatchRepository,
-  ) { }
+  ) {}
   async execute({ externalId, name }: ICreateBatchDTO): Promise<void> {
+    const beach = await this.batchRepository.findByName(name);
+    if (beach) throw new AppError('Lote já esta criado!');
     await this.batchRepository.create({ externalId, name });
   }
 }
